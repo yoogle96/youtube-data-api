e421b9a9d0b9425550830b8a484cfb08a3cc4940
Yu UiSeon <dmltjs851@gmail.com>
2020-05-27 19:59:08 +0900

    Add getVideos method

--- youtube-data-api/youtube_data.py
+++ youtube-data-api/youtube_data.py
@@ -1,7 +1,19 @@
+import requests, json, os
+
 class YoutubeDataAPI:
 
     def __init__(self):
         super().__init__()
 
+    def getVideos(self):
+        key = os.getenv('youtube_data_api_key')
+        endpoint = 'https://www.googleapis.com/youtube/v3/videos'
+        params = {'key' : key, 'part' : 'snippet', 'chart' : 'mostPopular', 'regionCode' : 'KR', 'maxResults' : '10'}
+        res = requests.get(endpoint, params).json()
+        print(json.dumps(res, indent=2, ensure_ascii=False))
+
+        
 if __name__ == "__main__":
+    a = YoutubeDataAPI()
+    a.getVideos()
     pass

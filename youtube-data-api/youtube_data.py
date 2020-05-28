85c46a8b1811a24b1f155b6d6513d075cf23506f
Yu UiSeon <dmltjs851@gmail.com>
2020-05-28 14:00:03 +0900

    Add getComments method

--- youtube-data-api/youtube_data.py
+++ youtube-data-api/youtube_data.py
@@ -8,12 +8,19 @@ class YoutubeDataAPI:
     def getVideos(self):
         key = os.getenv('youtube_data_api_key')
         endpoint = 'https://www.googleapis.com/youtube/v3/videos'
-        params = {'key' : key, 'part' : 'snippet', 'chart' : 'mostPopular', 'regionCode' : 'KR', 'maxResults' : '10'}
+        params = {'key' : key, 'part' : 'statistics,contentDetails', 'chart' : 'mostPopular', 'regionCode' : 'KR', 'maxResults' : '10'}
+        res = requests.get(endpoint, params).json()
+        print(json.dumps(res, indent=2, ensure_ascii=False))
+
+    def getComments(self):
+        key = os.getenv('youtube_data_api_key')
+        endpoint = 'https://www.googleapis.com/youtube/v3/comments'
+        params = {'key' : key, 'part' : 'snippet', 'parentId' : 'UgzDE2tasfmrYLyNkGt4AaABAg'}
         res = requests.get(endpoint, params).json()
         print(json.dumps(res, indent=2, ensure_ascii=False))
 
         
 if __name__ == "__main__":
     a = YoutubeDataAPI()
-    a.getVideos()
+    a.getComments()
     pass
